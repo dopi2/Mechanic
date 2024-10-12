74453f906fba75382966b872ccadf7f51a879f59
Frank Anya <frnkanya@gmail.com>
2024-10-12 13:44:47 -0700

    second commit

--- mechanic/src/main.jsx
+++ mechanic/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import Home from './Components/Home/home'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  
+  <Home/>
   </StrictMode>,
 )
